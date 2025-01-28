299e94e6bf5950e869b570cf6cb39129e957a53c
Heritage <ireanuoluwah@gmail.com>
2025-01-28 20:57:45 +0100

    ...

--- src/cart.jsx
+++ src/cart.jsx
@@ -4,7 +4,7 @@ import CartItem from './CartItem'
 import './index.css'
 const Cart = ({cart, addToCart}) => {
     let subTotal = 0
-    let deliveryFee = 15
+    let deliveryFee = 0
     let total = subTotal + deliveryFee
     console.log(cart)
 
@@ -13,6 +13,7 @@ const Cart = ({cart, addToCart}) => {
             <h1 className="bold" style={{marginLeft:'80px'}} > YOUR CART</h1>
             <div className="cart-container">
                 <div className="cart">
+                    {!cart.length && <h1 style={{textAlign:'center'}}>No item in cart yet</h1>}
                     {cart.map((item) => (<CartItem key={item.id} name={item.name} price={item.price} id={item.id} image={item.image} qty={item.qty} addToCart={addToCart} />))}
                 </div>
                 <div className="order-summary">
@@ -33,7 +34,6 @@ const Cart = ({cart, addToCart}) => {
                     <input type="button" value="Go to checkout " />
                 </div>
             </div>
-            
         </>
      );
 }
