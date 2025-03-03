103464e9ac502dc02751c8c24c0d0114347f528e
Heritage <ireanuoluwah@gmail.com>
2025-03-04 00:56:51 +0100

    changes to product page

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,11 +4,38 @@ import Item from "./item";
 import './index.css'
 
 const Product = ({products, cart, addToCart}) => {
-    let {id} = useParams()
+    let { id } = useParams();
+    let idNumber = Number(id);
+    let product = products.find((product) => product.id === idNumber);
+  
+
+  
     const mightLikeRef = useRef(null);
     return ( 
         
         <>
+            <div className="product-sec-1">
+                <div style={{display: "flex", flexDirection: "row-reverse", justifyContent:"flex-end", gap: "15px"}}>
+                    <div>
+                        <div className="product-image">
+                            <img src={`https://php-for-ecom-site.onrender.com/public/${product.image}`} alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className = "product-front small-image">
+
+                        </div>
+                        <div className = "product-back small-image">
+
+                        </div>
+                        <div className = "product-model small-image">
+
+                        </div>  
+                    </div>
+
+                </div>
+                
+            </div>
             <div className="product-sec-3">
                 <h2 className="bold">YOU MIGHT ALSO LIKE</h2>
                 <div className="carousel-container">
